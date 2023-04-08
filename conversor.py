b14d46436acc5ee1ec4482036667b93739f6e5e4
jrf00020 <jrf00020@red.ujaen.es>
2023-04-08 18:31:33 +0200

    First Commit

---
+++ conversor.py
@@ -0,0 +1,10 @@
+from pantalla_audio_to_audio import PantallaAudioToAudio
+from pantalla_menu import PantallaMenu
+from time import sleep
+from tkinter import *
+
+ventana = Tk()
+ventana.title('Conversor')
+ventana.config(width = 700,height = 500)
+
+ventana.mainloop()
